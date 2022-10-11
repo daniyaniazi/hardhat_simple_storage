e07ab20b78416a66302fb2c172b53086fe63462b
iamcsstudent123 <farihadania@hotmail.com>
2022-10-11 12:07:53 -0700

    verifying the contract after deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox")
 require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -11,6 +12,9 @@ module.exports = {
             accounts: [process.env.PRIVATE_KEY],
             chainId: 5,
         },
+    },
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
     },
 }
 
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,4 +1,4 @@
-const { ethers } = require("hardhat")
+const { ethers, run, network } = require("hardhat")
 
 async function main() {
     const SimpleStorageFactory = await ethers.getContractFactory(
@@ -8,6 +8,27 @@ async function main() {
     const simpleStorage = await SimpleStorageFactory.deploy()
     await simpleStorage.deployed()
     console.log("Contract deployed to : ", simpleStorage.address)
+    //verify
+    if (network.config.chainId == 5 && process.env.ETHERSCAN_API_KEY) {
+        await simpleStorage.deployTransaction.wait(6)
+        await verifyContract(simpleStorage.address, [])
+    }
+}
+
+async function verifyContract(contrcatAddress, args) {
+    console.log("Verifying contract")
+    try {
+        await run("verify:verify", {
+            address: contrcatAddress,
+            constructorArguments: args,
+        })
+    } catch (error) {
+        if (error.mesage.toLowerCase().includes("already verified")) {
+            console.log("Already Verified")
+        } else {
+            console.error(error)
+        }
+    }
 }
 
 main()
